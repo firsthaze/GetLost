2bb5f998ba7147f477303e8b6979ea07a3353570
Kaku <a87000012@gmail.com>
2017-05-26 02:40:33 +0800

    攝影機半成品

---
+++ GetLost/Assets/CameraRotater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotater : MonoBehaviour {
+
+	public float x;
+	public float y;
+	public float xSpeed = 100;
+	public float ySpeed = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x += Input.GetAxis ("Mouse X") * xSpeed * Time.deltaTime;
+		y -= Input.GetAxis ("Mouse Y") * ySpeed * Time.deltaTime;
+
+		if(x > 360) {
+			x -= 360;
+		} else if (x < 0){
+			x += 360;
+		}
+
+		transform.rotation = Quaternion.Euler (y, x, 0);
+	}
+}

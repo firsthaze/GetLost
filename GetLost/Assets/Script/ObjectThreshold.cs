3a986babc0c5e76bf744f5318303e5f19e897842
DESKTOP-SD50Q05\李修豪 <firsthaze@gmail.com>
2017-07-17 14:59:37 +0800

    場景布置

---
+++ GetLost/Assets/Script/ObjectThreshold.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectThreshold : MonoBehaviour {
+    public float _heightThreshold;
+    public float _weightThreshold;
+
+	// Use this for initialization
+	void Start () {
+        _heightThreshold = 0f;
+        _weightThreshold = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    float GetHeightThreshold()
+    {
+        return _heightThreshold;
+    }
+
+    float GetWeightThreshold()
+    {
+        return _weightThreshold;
+    }
+
+}

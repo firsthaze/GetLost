63f38a42b7ee28e9bd6ab3dbf315eea4e1612f48
DESKTOP-SD50Q05\李修豪 <firsthaze@gmail.com>
2017-08-22 15:31:46 +0800

    電容做功能上的修改 增加電量的限制

---
+++ GetLost/Assets/Script/CapacitanceOnce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapacitanceOnce : MonoBehaviour {
+
+    public float _remainGL;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float GetReaminGL()
+    {
+        return _remainGL;
+    }
+
+    public void SetRemainGL(float remainGL)
+    {
+        _remainGL = remainGL;
+    }
+}
